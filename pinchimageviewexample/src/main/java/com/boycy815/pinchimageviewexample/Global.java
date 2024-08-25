955a4c5ad08fc4a709aff09f892309a0bf464afb
boycy815 <boycy815@gmail.com>
2024-08-25 19:11:36 +0800

    fix zoomtransition demo bug

--- pinchimageviewexample/src/main/java/com/boycy815/pinchimageviewexample/Global.java
+++ pinchimageviewexample/src/main/java/com/boycy815/pinchimageviewexample/Global.java
@@ -26,6 +26,30 @@ public class Global {
                     new ImageObject("https://gw.alicdn.com/imgextra/i1/2211014865515/O1CN01oOpn301qbvFgAhTMa_!!2211014865515.jpg_240x10000Q75.jpg", 240, 320)
                     , new ImageObject("https://gw.alicdn.com/imgextra/i1/2211014865515/O1CN01oOpn301qbvFgAhTMa_!!2211014865515.jpg", 1200, 1600)
             )
+            , new ImageSourceImpl(
+                    new ImageObject("https://gw.alicdn.com/imgextra/i2/2211014865515/O1CN01CovDNr1qbvFbWPB4D_!!0-item_pic.jpg_240x10000Q75.jpg", 240, 320)
+                    , new ImageObject("https://gw.alicdn.com/imgextra/i2/2211014865515/O1CN01CovDNr1qbvFbWPB4D_!!0-item_pic.jpg", 1200, 1600)
+            )
+            , new ImageSourceImpl(
+                    new ImageObject("https://gw.alicdn.com/imgextra/i2/2211014865515/O1CN01OEkTUc1qbvFd4FSBy_!!2211014865515.jpg_240x10000Q75.jpg", 240, 320)
+                    , new ImageObject("https://gw.alicdn.com/imgextra/i2/2211014865515/O1CN01OEkTUc1qbvFd4FSBy_!!2211014865515.jpg", 1200, 1600)
+            )
+            , new ImageSourceImpl(
+                    new ImageObject("https://gw.alicdn.com/imgextra/i1/2211014865515/O1CN01oOpn301qbvFgAhTMa_!!2211014865515.jpg_240x10000Q75.jpg", 240, 320)
+                    , new ImageObject("https://gw.alicdn.com/imgextra/i1/2211014865515/O1CN01oOpn301qbvFgAhTMa_!!2211014865515.jpg", 1200, 1600)
+            )
+            , new ImageSourceImpl(
+                    new ImageObject("https://gw.alicdn.com/imgextra/i2/2211014865515/O1CN01CovDNr1qbvFbWPB4D_!!0-item_pic.jpg_240x10000Q75.jpg", 240, 320)
+                    , new ImageObject("https://gw.alicdn.com/imgextra/i2/2211014865515/O1CN01CovDNr1qbvFbWPB4D_!!0-item_pic.jpg", 1200, 1600)
+            )
+            , new ImageSourceImpl(
+                    new ImageObject("https://gw.alicdn.com/imgextra/i2/2211014865515/O1CN01OEkTUc1qbvFd4FSBy_!!2211014865515.jpg_240x10000Q75.jpg", 240, 320)
+                    , new ImageObject("https://gw.alicdn.com/imgextra/i2/2211014865515/O1CN01OEkTUc1qbvFd4FSBy_!!2211014865515.jpg", 1200, 1600)
+            )
+            , new ImageSourceImpl(
+                    new ImageObject("https://gw.alicdn.com/imgextra/i1/2211014865515/O1CN01oOpn301qbvFgAhTMa_!!2211014865515.jpg_240x10000Q75.jpg", 240, 320)
+                    , new ImageObject("https://gw.alicdn.com/imgextra/i1/2211014865515/O1CN01oOpn301qbvFgAhTMa_!!2211014865515.jpg", 1200, 1600)
+            )
     };
 
     public static ImageSource getTestImage(int i) {
--- pinchimageviewexample/src/main/java/com/boycy815/pinchimageviewexample/images/ImageObject.java
+++ pinchimageviewexample/src/main/java/com/boycy815/pinchimageviewexample/images/ImageObject.java
@@ -1,11 +1,13 @@
 package com.boycy815.pinchimageviewexample.images;
 
+import java.io.Serializable;
+
 /**
  * 图片信息结构体
  *
  * @author clifford
  */
-public class ImageObject {
+public class ImageObject implements Serializable {
     public int width;
     public int height;
     public String url;
