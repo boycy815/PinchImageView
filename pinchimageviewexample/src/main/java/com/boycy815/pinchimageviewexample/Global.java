9da8251da58169a73856ecde898b556dcc702826
boycy815 <boycy815@gmail.com>
2024-08-28 14:05:28 +0800

    update demo images

--- pinchimageviewexample/src/main/java/com/boycy815/pinchimageviewexample/Global.java
+++ pinchimageviewexample/src/main/java/com/boycy815/pinchimageviewexample/Global.java
@@ -27,28 +27,28 @@ public class Global {
                     , new ImageObject("https://gw.alicdn.com/imgextra/i1/2211014865515/O1CN01oOpn301qbvFgAhTMa_!!2211014865515.jpg", 1200, 1600)
             )
             , new ImageSourceImpl(
-                    new ImageObject("https://gw.alicdn.com/imgextra/i2/2211014865515/O1CN01CovDNr1qbvFbWPB4D_!!0-item_pic.jpg_240x10000Q75.jpg", 240, 320)
-                    , new ImageObject("https://gw.alicdn.com/imgextra/i2/2211014865515/O1CN01CovDNr1qbvFbWPB4D_!!0-item_pic.jpg", 1200, 1600)
+                    new ImageObject("https://img.alicdn.com/imgextra/i4/2635590370/O1CN01OpR4Dg1EbVWGcLnII_!!2635590370.jpg_240x10000Q75.jpg", 240, 575)
+                    , new ImageObject("https://img.alicdn.com/imgextra/i4/2635590370/O1CN01OpR4Dg1EbVWGcLnII_!!2635590370.jpg", 750, 1796)
             )
             , new ImageSourceImpl(
-                    new ImageObject("https://gw.alicdn.com/imgextra/i2/2211014865515/O1CN01OEkTUc1qbvFd4FSBy_!!2211014865515.jpg_240x10000Q75.jpg", 240, 320)
-                    , new ImageObject("https://gw.alicdn.com/imgextra/i2/2211014865515/O1CN01OEkTUc1qbvFd4FSBy_!!2211014865515.jpg", 1200, 1600)
+                    new ImageObject("https://img.alicdn.com/imgextra/i1/2635590370/O1CN01DwIP0s1EbVVyBItDr_!!2635590370.jpg_240x10000Q75.jpg", 240, 288)
+                    , new ImageObject("https://img.alicdn.com/imgextra/i1/2635590370/O1CN01DwIP0s1EbVVyBItDr_!!2635590370.jpg", 1800, 2160)
             )
             , new ImageSourceImpl(
-                    new ImageObject("https://gw.alicdn.com/imgextra/i1/2211014865515/O1CN01oOpn301qbvFgAhTMa_!!2211014865515.jpg_240x10000Q75.jpg", 240, 320)
-                    , new ImageObject("https://gw.alicdn.com/imgextra/i1/2211014865515/O1CN01oOpn301qbvFgAhTMa_!!2211014865515.jpg", 1200, 1600)
+                    new ImageObject("https://img.alicdn.com/imgextra/i1/2635590370/O1CN016l4UE11EbVVyV8BwT_!!2635590370.jpg_240x10000Q75.jpg", 240, 288)
+                    , new ImageObject("https://img.alicdn.com/imgextra/i1/2635590370/O1CN016l4UE11EbVVyV8BwT_!!2635590370.jpg", 1800, 2160)
             )
             , new ImageSourceImpl(
-                    new ImageObject("https://gw.alicdn.com/imgextra/i2/2211014865515/O1CN01CovDNr1qbvFbWPB4D_!!0-item_pic.jpg_240x10000Q75.jpg", 240, 320)
-                    , new ImageObject("https://gw.alicdn.com/imgextra/i2/2211014865515/O1CN01CovDNr1qbvFbWPB4D_!!0-item_pic.jpg", 1200, 1600)
+                    new ImageObject("https://img.alicdn.com/imgextra/i1/2635590370/O1CN016l4UE11EbVVyV8BwT_!!2635590370.jpg_240x10000Q75.jpg", 240, 288)
+                    , new ImageObject("https://img.alicdn.com/imgextra/i1/2635590370/O1CN016l4UE11EbVVyV8BwT_!!2635590370.jpg", 1800, 2160)
             )
             , new ImageSourceImpl(
-                    new ImageObject("https://gw.alicdn.com/imgextra/i2/2211014865515/O1CN01OEkTUc1qbvFd4FSBy_!!2211014865515.jpg_240x10000Q75.jpg", 240, 320)
-                    , new ImageObject("https://gw.alicdn.com/imgextra/i2/2211014865515/O1CN01OEkTUc1qbvFd4FSBy_!!2211014865515.jpg", 1200, 1600)
+                    new ImageObject("https://img.alicdn.com/imgextra/i4/2635590370/O1CN01dgbTFB1EbVVyV6a9T_!!2635590370.jpg_240x10000Q75.jpg", 240, 288)
+                    , new ImageObject("https://img.alicdn.com/imgextra/i4/2635590370/O1CN01dgbTFB1EbVVyV6a9T_!!2635590370.jpg", 1800, 2160)
             )
             , new ImageSourceImpl(
-                    new ImageObject("https://gw.alicdn.com/imgextra/i1/2211014865515/O1CN01oOpn301qbvFgAhTMa_!!2211014865515.jpg_240x10000Q75.jpg", 240, 320)
-                    , new ImageObject("https://gw.alicdn.com/imgextra/i1/2211014865515/O1CN01oOpn301qbvFgAhTMa_!!2211014865515.jpg", 1200, 1600)
+                    new ImageObject("https://img.alicdn.com/imgextra/i2/2635590370/O1CN01Kbhx6j1EbVW0LcVqW_!!2635590370.jpg_240x10000Q75.jpg", 240, 288)
+                    , new ImageObject("https://img.alicdn.com/imgextra/i2/2635590370/O1CN01Kbhx6j1EbVW0LcVqW_!!2635590370.jpg", 1800, 2160)
             )
     };
 
